eb8b78decbb347a75642041a0b833a8390aab118
shimizu0124 <s.ch26.gt.jxd@gmail.com>
2017-11-17 15:32:26 +0900

    動的に変化する値の処理（全体-日次 / ヘルプ集計）

--- WebWorkSystem/WebWorker/Controllers/HelpController.cs
+++ WebWorkSystem/WebWorker/Controllers/HelpController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using WebWorker.ViewModels.Help;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,34 @@ namespace WebWorker.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new HelpListViewModel();
+
+            viewModel.WorkDay = "3/1";
+            viewModel.Name = "青山大地";
+            viewModel.StartTime = "06:00";
+            viewModel.BreakStartTime = "10:00";
+            viewModel.BreakEndTime = "11:00";
+            viewModel.EndTime = "14:00";
+            viewModel.WorkTime = "07:00";
+            viewModel.Help = "町田";
+
+            return View(viewModel);
         }
 
         public ActionResult Edit()
         {
-            return View();
+            var viewModel = new HelpListViewModel();
+
+            viewModel.WorkDay = "3/1";
+            viewModel.Name = "青山大地";
+            viewModel.StartTime = "06:00";
+            viewModel.BreakStartTime = "10:00";
+            viewModel.BreakEndTime = "11:00";
+            viewModel.EndTime = "14:00";
+            viewModel.WorkTime = "07:00";
+            viewModel.Help = "町田";
+
+            return View(viewModel);
         }
 
     }
